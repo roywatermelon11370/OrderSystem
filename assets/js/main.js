8dfef0cfdb4626d53dac28498913d534a698af2e
roywatermelon11370 <53699625+roywatermelon11370@users.noreply.github.com>
2019-10-03 23:07:07 +0800

    yeah

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){ 
+	$(window).on('scroll', function() {
+        if($(window).scrollTop()) {
+            $('#navbar').removeClass('alt');
+        }
+        else {
+            $('#navbar').addClass('alt');
+        }
+    })
+
+    $()
+});
