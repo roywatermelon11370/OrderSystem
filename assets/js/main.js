7909f6fd443d79b02d2e5f1d591ef4cdab921cad
roywatermelon11370 <53699625+roywatermelon11370@users.noreply.github.com>
2019-11-25 20:28:52 +0800

    o

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,15 @@ $(document).ready(function(){
 	$(window).on('scroll', function() {
         if($(window).scrollTop()) {
             $('#navbar').removeClass('alt');
+            $('#navbar-normal').removeClass('bg-light');
+            $('#navbar-normal').addClass('bg-white');
+            $('#navbar-normal').addClass('nav-shadow');
         }
         else {
             $('#navbar').addClass('alt');
+            $('#navbar-normal').removeClass('bg-white');
+            $('#navbar-normal').removeClass('nav-shadow');
+            $('#navbar-normal').addClass('bg-light');
         }
     })
 
